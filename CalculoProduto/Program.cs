84a9f217990919243c21e15a1a4ffbcca243503b
Izabella Aro <izabella.aro20@gmail.com>
2024-02-27 15:33:28 -0300

    Adiciona configs BD

--- CalculoProduto/Program.cs
+++ CalculoProduto/Program.cs
@@ -1,10 +1,20 @@
 using CalculoProduto.Application.Services;
 using CalculoProduto.Application.Services.Impl;
+using CalculoProduto.DataAccess;
+using CalculoProduto.DataAccess.Repositories;
+using CalculoProduto.DataAccess.Repositories.Impl;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("PrecificaProdConnection");
+builder.Services.AddDbContext<ProdutoContext>(opts => opts.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
 // Add services to the container.
 builder.Services.AddScoped<IProdutoService, ProdutoService>();
+
+// Add repositories
+builder.Services.AddScoped<IInsumoIndiretoRepository, InsumoIndiretoRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
